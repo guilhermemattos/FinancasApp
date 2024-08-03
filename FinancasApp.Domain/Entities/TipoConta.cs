7b05cccc74ecb21a913f2cb63cd6ff96dd49835b
guilhermemattos <guilherme.lial@gmail.com>
2024-08-03 15:20:50 -0300

    first commit

---
+++ FinancasApp.Domain/Entities/TipoConta.cs
@@ -0,0 +1,8 @@
+﻿namespace FinancasApp.Domain.Entities
+{
+    public enum TipoConta
+    {
+        Receber = 1,
+        Pagar = 2
+    }
+}
---
+++ FinancasApp.Tests/ContasTest.cs
@@ -0,0 +1,35 @@
+﻿namespace FinancasApp.Tests
+{
+    public class ContasTest
+    {
+        [Fact(Skip = "Não implementado.")]
+        public void Post_Contas_Returns_Created()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public void Put_Contas_Returns_Ok()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public void Delete_Contas_Returns_Ok()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public void GetByDatas_Contas_Returns_Ok()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public void GetById_Contas_Returns_Ok()
+        {
+
+        }
+    }
+}
